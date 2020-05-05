a3300b2ca555eb50552d198eb920251c5b4f6abd
Vitalii <57675474+silens088@users.noreply.github.com>
2020-05-05 17:28:26 +0300

    Update TestUserAccessData.java

--- src/test/java/RequestGet/TestUserAccessData.java
+++ src/test/java/RequestGet/TestUserAccessData.java
@@ -10,9 +10,9 @@ class TestUserAccessData {
     void user1() {
         RequestGet request1 = new RequestGet();
 
-        request1.requestGet("globaldb.com/company/100/users?name=Vitalii", "zxcvbnm"); //Аутентификация выполнена. 200 OK
+        request1.requestGet("globaldb.com/company/100/users?name=Vitalii", "zxcvbnm"); //Пользователь user1: аутентификация выполнена. Данные получены.
 
-        Assert.assertEquals(200, request1.getCode()); //Пользователь user1: аутентификация выполнена. Данные получены.
+        Assert.assertEquals(200, request1.getCode()); 
     }
 
     @Test
@@ -20,8 +20,8 @@ class TestUserAccessData {
     void user2() {
         RequestGet request2 = new RequestGet();
 
-        request2.requestGet("globaldb.com/company/100/users?name=Vitalii", "mnbvcxz");
+        request2.requestGet("globaldb.com/company/100/users?name=Vitalii", "mnbvcxz"); //Пользователь user2: аутентификация выполнена, нет доступа к данным. 403 (Forbidden)
 
-        Assert.assertEquals(403, request2.getCode()); //Пользователь user2: аутентификация выполнена, нет доступа к данным. 403 (Forbidden)
+        Assert.assertEquals(403, request2.getCode()); 
     }
 }
